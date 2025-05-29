998ad7adce03b9372da3472fafafd47cab06f357
BTCLNAT <yuridelgado773@gmail.com>
2025-05-28 21:03:50 -0400

    Create se.js

---
+++ se.js
@@ -0,0 +1,40 @@
+const CACHE_NAME = "bitmed-family-v1";
+const urlsToCache = [
+  "/",
+  "/index.html",
+  "/manifest.json",
+  "/icon-192.png",
+  "/icon-512.png",
+  "/style.css" // agrega aquí otros archivos estáticos si los usas
+];
+
+// Instalación: cachear archivos esenciales
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache))
+  );
+});
+
+// Activación: limpieza de cachés antiguas
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) =>
+      Promise.all(
+        cacheNames.map((name) => {
+          if (name !== CACHE_NAME) {
+            return caches.delete(name);
+          }
+        })
+      )
+    )
+  );
+});
+
+// Intercepción de solicitudes: servir desde caché si es posible
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cached) => {
+      return cached || fetch(event.request);
+    })
+  );
+});
